bb29792de53aad881ad247173b202cb5afad46fe
moussaif <mmoussaif@MacBook-Pro.local>
2020-11-03 15:14:22 -0500

    Fixing producer and consumer python code

--- Kafka/producer.py
+++
@@ -1,7 +0,0 @@
-from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
-with open('inputfile.txt') as f:
-     lines = f.readlines()
-
-for line in lines:
-   producer.send('test', line)
