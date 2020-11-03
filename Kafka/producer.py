0be36110e6c936f6994bb0b9fd97de5eb9aeaf4a
moussaif <mmoussaif@MacBook-Pro.local>
2020-11-03 00:26:11 -0500

    update file

--- Kafka/producer.py
+++ Kafka/producer.py
@@ -1,6 +1,6 @@
 from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
-with Open (./inputfile.txt') as f:
+with open('inputfile.txt') as f:
      lines = f.readlines()
 
 for line in lines:
